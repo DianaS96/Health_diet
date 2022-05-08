d6f29449846857e9351e0eb702bf6478b6605582
Diana Sibagatullina <diana.sibagatullina96@gmail.com>
2022-05-09 01:36:28 +0300

    app.py - changed (graphs)

--- app.py
+++ app.py
@@ -10,10 +10,17 @@ import sqlite3
 from datetime import datetime, date
 import pandas as pd
 from sqlalchemy import func
+import plotly as py
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import json
+from utils import get_totals_PFC
 
 app = Flask(__name__)
 
-#The database URI that should be used for the connection.
+
+# The database URI that should be used for the connection.
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_BINDS'] = {
     'users_info':   'sqlite:///users_info.db'
@@ -27,11 +34,13 @@ login_manager.init_app(app)
 
 db.init_app(app)
 
-#create the USER table and database
+
+# create the USER table and database
 @app.before_first_request
 def create_table():
     db.create_all(app=app)
     db.create_all(bind='users_info')
+
 
 # Info about SECRET_KEY usage: https://flask.palletsprojects.com/en/2.1.x/config/
 # Flask-Login uses sessions for authentication,
@@ -41,22 +50,26 @@ def create_table():
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 
+
 # A user loader tells Flask-Login how to get a specific user object
 # from the ID that is stored in the session cookie
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
 
+
 def get_products_connected():
     conn = sqlite3.connect('products.db')
     conn.row_factory = sqlite3.Row
     return conn
 
+
 # Returning homepage template
 @app.route('/')
 @app.route('/index')
 def home():
-    return login()
+    return render_template('index.html')
+
 
 # Register
 @app.route('/register', methods=['POST', 'GET'])
@@ -75,6 +88,7 @@ def register():
         return redirect(url_for('login'))
     return render_template('registration.html', form=form)
 
+
 # Login
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -87,6 +101,7 @@ def login():
             return redirect(next or url_for('home'))
         flash("Invalid email or password!")
     return render_template('login.html', form=form)
+
 
 # Logout
 @app.route('/logout', methods=['POST', 'GET'])
@@ -103,9 +118,6 @@ def products_table():
     prod = conn.execute('SELECT * FROM products').fetchall()
     conn.close()
     return render_template('products_table.html', prod=prod)
-
-"""
-"""
 
 
 @app.route('/diary_add', methods=['POST', 'GET'])
@@ -138,6 +150,7 @@ def diary_add():
         return redirect(url_for('diary_add', form=form))
     return render_template('diary_add.html', form=form)
 
+
 @app.route('/diary_show', methods=['POST', 'GET'])
 #@login_required
 def diary_show():
@@ -151,27 +164,56 @@ def diary_show():
         d = Users_info.query.filter_by(user=current_user.username, date_str=form_date.date.data).order_by(Users_info.date.desc()).all()
     #    conn.close()
 
-        sum_amount = Users_info.query.filter_by(user=current_user.username, date_str=form_date.date.data).\
-            with_entities(func.sum(Users_info.amount).label('total')).first().total
+        sum_amount, sum_cal, sum_prot, sum_fats, sum_co2 = get_totals_PFC(form_date)
 
-        sum_cal = Users_info.query.filter_by(user=current_user.username, date_str=form_date.date.data).\
-            with_entities(func.sum(Users_info.calories).label('total')).first().total
+        product_amount_per_day = db.session.query(Users_info.product, db.func.sum(Users_info.amount)).\
+            filter_by(user=current_user.username, date_str=form_date.date.data).\
+            group_by(Users_info.product).all()
 
-        sum_prot = Users_info.query.filter_by(user=current_user.username, date_str=form_date.date.data).\
-            with_entities(func.sum(Users_info.proteins).label('total')).first().total
+        type_amount_per_day = db.session.query(Users_info.type, db.func.sum(Users_info.amount)).\
+            filter_by(user=current_user.username, date_str=form_date.date.data).\
+            group_by(Users_info.type).all()
 
-        sum_fats = Users_info.query.filter_by(user=current_user.username, date_str=form_date.date.data).\
-            with_entities(func.sum(Users_info.fats).label('total')).first().total
+        df_product = pd.DataFrame(data=product_amount_per_day, columns=['Product', 'Amount'])
+        df_type = pd.DataFrame(data=type_amount_per_day, columns=['Type', 'Amount'])
+        print(df_type)
 
-        sum_co2 = Users_info.query.filter_by(user=current_user.username, date_str=form_date.date.data).\
-            with_entities(func.sum(Users_info.carbohydrates).label('total')).first().total
+        fig = make_subplots(rows=1, cols=1, specs=[[{'type':'domain'}]])
+
+        fig.add_trace(go.Pie(labels=df_type['Type'],
+                             values=df_type['Amount'],
+                             name='Breakdown by type of product', legendgroup=1), 1, 1)
+        #fig.add_trace(go.Pie(labels=df_product['Product'],
+         #                    values=df_product['Amount'],
+          #                   name='Breakdown by product', legendgroup=2), 1, 2)
+
+        fig.update_traces(hole=.4, hoverinfo="label+percent+name")
+
+        fig.update_layout(
+            title_text="Breakdown of meals by type and product, %",
+            # Add annotations in the center of the donut pies.
+            annotations=[dict(text='Type', x=0.18, y=0.5, font_size=20, showarrow=False)])
+
+        graphJSON = json.dumps(fig, cls=py.utils.PlotlyJSONEncoder)
+
+        sum_PFC = sum_prot + sum_cal + sum_co2
+
+        labels = ['Proteins', 'Fats', 'Carbohydrates']
+        values = [(round(float(sum_prot / sum_PFC), 2)),
+                  (round(float(sum_fats / sum_PFC), 2)),
+                  (round(float(sum_co2 / sum_PFC), 2))]
+
+        fig1 = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
+        graphJSON1 = json.dumps(fig1, cls=py.utils.PlotlyJSONEncoder)
 
         return render_template('diary_show.html', date=form_date, user=d,
                                sum_cal=round(sum_cal, 2),
                                sum_amount=round(sum_amount, 2),
                                sum_prot=round(sum_prot, 2),
                                sum_fats=round(sum_fats, 2),
-                               sum_co2=round(sum_co2, 2))
+                               sum_co2=round(sum_co2, 2),
+                               plot_pie_chart_products=graphJSON,
+                               plot_pie_chart_pfc=graphJSON1)
     return render_template('diary_show.html', date=form_date)
 
 
@@ -188,6 +230,7 @@ def product(types):
         prod_arr.append(prObj)
 
     return flask.json.jsonify({'prod_arr':prod_arr})
+
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8000)
